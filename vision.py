e3b9ce5d8ca32a604e2584e0ef63d91401a1c9df
Oliver Allshire <oliver.allshire@gmail.com>
2018-01-26 11:27:41 +1100

    Changed the angle calculation

--- vision.py
+++ vision.py
@@ -77,8 +77,8 @@ def process(frame):
 
                 distance = width - mask_width / 2
                 # convert x-axis location to -1 to 1
-                position = -distance / (mask_width / 2)
-                angle = math.atan(position / focal_length)
+                #position = -distance / (mask_width / 2)
+                angle = math.atan(-distance / focal_length)
 
                 output.extend([angle, position, cv2.contourArea(contour)])
 
