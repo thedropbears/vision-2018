90f11bae1913eb47e029d11012228796585d8606
Oliver Allshire <oliver.allshire@gmail.com>
2018-01-28 14:49:13 +1100

    fixed the memory allocation issue, made the size parameters proportional to the resolution

--- vision.py
+++ vision.py
@@ -10,7 +10,8 @@ yellowUpper = (35, 255, 255)
 
 frame_width = 320
 frame_height = 240
-min_radius = frame_height * 0.125
+min_radius = frame_height * 0.0625
+min_area = frame_width * 0.0625
 fov = 75
 focal_length = 208.5
 fps = 50
@@ -22,12 +23,11 @@ def loop():
     camera.setVideoMode(cscore.VideoMode.PixelFormat.kYUYV, 320, 240, 60)
     sink = cs.getVideo(camera=camera)
 
-    NetworkTables.initialize(server="roborio-4774-frc.local")
     nt = NetworkTables.getTable('/vision')
     entry = nt.getEntry('info')
-    
+    frame = np.zeros(shape=(frame_height, frame_width, 3), dtype=np.uint8)
+
     while True:
-        frame = np.zeros(shape=(frame_height, frame_width, 3), dtype=np.uint8)
         info = process(sink.grabFrame(frame)[1])
         info = np.array(info).flatten()
         entry.setNumberArray(info)
@@ -38,39 +38,40 @@ def process(frame):
     start_time = time.monotonic()
 
 #     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    bgr = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 
     # construct a mask for the colour "yellow", then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
     mask = cv2.inRange(hsv, yellowLower, yellowUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    cv2.erode(mask, None, mask, iterations=2)
+    cv2.dilate(mask, None, mask, iterations=2)
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
-    output = None
-    angle = None
-    
+
     output = []
-    # only proceed if at least one contour was found
+    # only proceed if at least one contour was founda
     if contours:
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        contours.sort(key=cv2.contourArea, reverse=True)
+        contours.sort(key=cv2.contourArea, reverse=True)]
+
+        
 
         for contour in contours:
             # contour = max(contours, key=cv2.contourArea)
+            
+            contour_area = cv2.contourArea(contour)
+
             ((x, y), radius) = cv2.minEnclosingCircle(contour)
             M = cv2.moments(contour)
 
         # only proceed if the radius meets a minimum size
-            if cv2.contourArea(contour) > (radius * 0.7)**2 and radius > min_radius:
+            if contour_area > min_area  and radius > min_radius:
                 width, height = center = (
                     int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
                 mask_height, mask_width = mask.shape  # find resolution of mask
@@ -80,7 +81,7 @@ def process(frame):
                 #position = -distance / (mask_width / 2)
                 angle = math.atan(-distance / focal_length)
 
-                output.extend([angle, position, cv2.contourArea(contour)])
+                output.extend([angle, position, contour_area])
 
     end_time = time.monotonic() - start_time
     output.append(end_time)
