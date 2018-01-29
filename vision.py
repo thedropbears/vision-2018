1dbbaf4dc0562854e4b01403e4b47605272c2d50
David Vo <auscompgeek@users.noreply.github.com>
2018-01-29 20:26:38 +1100

    Clean up and optimise vision code

--- vision.py
+++ vision.py
@@ -1,58 +1,57 @@
-import cscore
-import numpy as np
-import cv2
-from networktables import NetworkTables
-import time
 import math
 
-yellowLower = (15, 100, 100)
-yellowUpper = (35, 255, 255)
-
-frame_width = 320
-frame_height = 240
-min_radius = frame_height * 0.0625
-min_area = frame_width * 0.0625
-fov = 75
-focal_length = 208.5
-fps = 50
+import cv2
+import numpy as np
 
 
-def loop():
+def main():
+    import cscore
+    from networktables import NetworkTables
+    from time import monotonic
+
     cs = cscore.CameraServer.getInstance()
     camera = cs.startAutomaticCapture()  # TODO: specify path if multiple cameras
-    camera.setVideoMode(cscore.VideoMode.PixelFormat.kYUYV, 320, 240, 60)
+    camera.setVideoMode(cscore.VideoMode.PixelFormat.kYUYV, 320, 240, 50)
     sink = cs.getVideo(camera=camera)
 
     nt = NetworkTables.getTable('/vision')
     entry = nt.getEntry('info')
-    frame = np.zeros(shape=(frame_height, frame_width, 3), dtype=np.uint8)
+
+    # Allocating memory is expensive. Preallocate an array for the camera image.
+    frame = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
 
     while True:
-        info = process(sink.grabFrame(frame)[1])
-        info = np.array(info).flatten()
-        entry.setNumberArray(info)
-        NetworkTables.flush()
+        time, frame = sink.grabFrame(frame)
+        if time == 0:
+            # TODO: handle error
+            pass
+        else:
+            start_time = monotonic()
+            info = process(frame)
+            info.append(monotonic() - start_time)
+
+            entry.setNumberArray(info)
+            NetworkTables.flush()
 
 
-def process(frame):
-    start_time = time.monotonic()
+def process(frame, lower=(15, 100, 100), upper=(35, 255, 255),
+            min_radius_prop=0.0625, min_area_prop=0.0625,
+            fov=75, focal_length=208.5):
 
-#     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
 
     # construct a mask for the colour "yellow", then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
-    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+    mask = cv2.inRange(hsv, lower, upper)
     cv2.erode(mask, None, mask, iterations=2)
     cv2.dilate(mask, None, mask, iterations=2)
 
-    # find resolution of mask
-    mask_height, mask_width = mask.shape
+    # find resolution of mask/image
+    height, width = mask.shape
 
-    min_radius = mask_height * 0.0625
-    min_area = mask_width * 0.0625
+    min_radius = height * min_radius_prop
+    min_area = width * min_area_prop
 
     # find contours in the mask and initialize the current
     # x center of the cube
@@ -66,35 +65,23 @@ def process(frame):
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        contours.sort(key=cv2.contourArea, reverse=True)]
-
-        
+        contours.sort(key=cv2.contourArea, reverse=True)
 
         for contour in contours:
-            # contour = max(contours, key=cv2.contourArea)
-            
             contour_area = cv2.contourArea(contour)
 
             ((x, y), radius) = cv2.minEnclosingCircle(contour)
             M = cv2.moments(contour)
 
-        # only proceed if the radius meets a minimum size
-            if contour_area > min_area  and radius > min_radius:
+            # only proceed if the radius meets a minimum size
+            if contour_area > min_area and radius > min_radius:
+                x = M["m10"] / M["m00"]
 
-                x = int(M["m10"] / M["m00"])
-
-                distance = x - mask_width / 2
+                distance = x - width / 2
                 # convert x-axis location to -1 to 1
-                #position = -distance / (mask_width / 2)
+                position = -distance / (width / 2)
                 angle = math.atan(-distance / focal_length)
 
                 output.extend([angle, position, contour_area])
 
-    end_time = time.monotonic() - start_time
-    output.append(end_time)
-
     return output
-
-
-if __name__ == '__main__':
-    loop()
